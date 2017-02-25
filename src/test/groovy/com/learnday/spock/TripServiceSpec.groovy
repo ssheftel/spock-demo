a5650b7c07410b1768451ba58681a1eac3df8db1
Sam Sheftel <mathias@mailinator.com>
2017-02-25 17:34:56 -0600

    added spock + notes on steps

--- src/test/groovy/com/learnday/spock/TripServiceSpec.groovy
+++ src/test/groovy/com/learnday/spock/TripServiceSpec.groovy
@@ -1,21 +1,3 @@
-# Spock Demo Notes
-
-## Demo 1. Adding Spock to a Spring Boot Project
-
-branch name: `demo-1-adding-spock`
-
-1. In build.gradle add the groovy plugin `apply plugin: 'groovy'` and the following test dependencies
-
-```groovy
-	testCompile 'org.spockframework:spock-spring:1.0-groovy-2.4'
-	testCompile 'cglib:cglib-nodep:3.2.2'
-	testCompile 'com.blogspot.toomuchcoding:spock-subjects-collaborators-extension:1.2.1'
-```
-2. Create a new directory named "groovy" in src/test
-3. Create the packages matching your projects package name e.g. `com.learnday.spock`
-4. Create new test specifications files. These files should have Spec as a suffix and have the .groovy extension.
-
-```groovy
 package com.learnday.spock
 
 import spock.lang.*
@@ -42,4 +24,3 @@ class TripServiceSpec extends Specification {
             savedTripDto.tripId == 123
     }
 }
-```
